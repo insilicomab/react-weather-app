ef24c518e78514c867f4f9eb821701497b3c6772
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-08-30 00:43:01 +0900

    make Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Form = () =>{
     const [city, setCity] = useState("");
+    const getWeather = (e) => {
+        e.preventDefault();
+        axios.get("http://api.weatherapi.com/v1/current.json?key=53535532cc1b4eada6c125622212908&q=London&aqi=no").then(res=>console.log(res))
+    }
     return (
         <form>
             <input type="text" name="city" placeholder="都市名" onChange={e=>setCity(e.target.value)} />
-            {city}
-            <button type="submit">Get Weather</button>
+            
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
