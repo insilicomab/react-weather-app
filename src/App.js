403624dd07717f7dc76cf6117270bc488a72471b
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-08-29 21:07:07 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Title from './components/Title';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
