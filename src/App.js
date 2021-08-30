0163668fc4d768fac58af437b7f12975892b576f
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-08-30 11:43:59 +0900

    Add script

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Title from './components/Title';
 import Form from './components/Form';
 import './App.css';
+import Results from './components/Results';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("");
+    const getWeather = (e) => {
+        e.preventDefault();
+        axios.get("http://api.weatherapi.com/v1/current.json?key=53535532cc1b4eada6c125622212908&q=London&aqi=no").then(res=>console.log(res))
+    }
   return (
-    <div className="App">
+    <div className="test">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import axios from "axios";
-
-const Form = () =>{
-    const [city, setCity] = useState("");
-    const getWeather = (e) => {
-        e.preventDefault();
-        axios.get("http://api.weatherapi.com/v1/current.json?key=53535532cc1b4eada6c125622212908&q=London&aqi=no").then(res=>console.log(res))
-    }
+const Form = (props) =>{
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e=>setCity(e.target.value)} />
+            <input type="text" name="city" placeholder="都市名" onChange={e=>props.setCity(e.target.value)} />
             
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
 };
