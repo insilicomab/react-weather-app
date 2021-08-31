6ccdc625f56b8d417ef23cee696aa515d6540de1
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-08-31 10:55:08 +0900

    add script

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 const Form = (props) =>{
     return (
         <form onSubmit={props.getWeather}>
-            <input type="text" name="city" placeholder="都市名" onChange={e=>props.setCity(e.target.value)} />
+            <input type="text" name="city" placeholder="都市名" onChange={e=>props.setCity(e.target.value)} value={props.city}/>
             
             <button type="submit">Get Weather</button>
         </form>
